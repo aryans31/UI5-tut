5915cf8ddf8ee9f5831c7b108553fdbe07dcfe28
Aryan Saxena <aryan.saxena3105@gmail.com>
2022-07-01 12:28:03 +0530

    step 2 UI5 tut

--- webapp/index.js
+++ webapp/index.js
@@ -1,18 +1,15 @@
 sap.ui.define([
-	"sap/m/Button",
-	"sap/m/MessageToast"
+	"sap/ui/core/mvc/XMLView"
 ],
 
-function (Button, MessageToast)
+function (XMLView)
  {
 	"use strict";
 
-	new Button({
-		text: "Ready...",
-		press: function () {
-			MessageToast.show("Hello World!");
-		}
-	}).placeAt("content");
+	alert("UI5 is ready")
 
+	// XMLView.create({viewName: "Quickstart.App"}).then(function(oView){
+	// 	oView.placeAt("content");
+	// });
 }
 );
